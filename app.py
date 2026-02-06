258f5789de1bc7f8b3ad136bd8c87d0e19019a49
ProsperAdeoul <prosperadeoul@gmail.com>
2026-02-06 13:45:03 +0000

    Ajout de Hybrid Technique

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
 # Chargement des données
@@ -11,39 +12,73 @@ def load_data():
 
 movies, ratings = load_data()
 
-# Création de la matrice collaborative
+@st.cache_data
+def build_tfidf(movies):
+    tfidf = TfidfVectorizer(stop_words='english')
+    tfidf_matrix = tfidf.fit_transform(movies['genres'])
+
+    cosine_sim = cosine_similarity(tfidf_matrix, tfidf_matrix)
+    return cosine_sim
+
+cosine_sim = build_tfidf(movies)
+
 @st.cache_data
 def build_similarity(ratings):
-    movie_user_matrix = ratings.pivot_table(
-        index='movieId',
-        columns='userId',
+    user_movie_matrix = ratings.pivot_table(
+        index='userId',
+        columns='movieId',
         values='rating'
     ).fillna(0)
 
-    similarity = cosine_similarity(movie_user_matrix)
-    
-    similarity_df = pd.DataFrame(
-        similarity,
-        index=movie_user_matrix.index,
-        columns=movie_user_matrix.index
+    movie_similarity = cosine_similarity(user_movie_matrix.T)
+
+    movie_similarity_df = pd.DataFrame(
+        movie_similarity,
+        index=user_movie_matrix.columns,
+        columns=user_movie_matrix.columns
     )
-    return similarity_df
+    return movie_similarity_df
 
 movie_similarity_df = build_similarity(ratings)
 
 # Fonction de recommandation
 
-def recommend_movies_collaborative(movie_title, n=10):
+def recommend_movies_hybrid(movie_title, n=10, w_content=0.5, w_collaborative=0.5):
 
     if movie_title not in movies['title'].values:
-        return None
+        return "Film non trouvé"
 
-    movie_id = movies[movies['title'] == movie_title]['movieId'].values[0]
+    # index pour content-based
+    movie_idx = movies[movies['title'] == movie_title].index[0]
 
-    sim_scores = movie_similarity_df[movie_id].sort_values(ascending=False)
-    sim_scores = sim_scores.iloc[1:n+1]
+    # movieId pour collaborative
+    movie_id = movies.loc[movie_idx, 'movieId']
 
-    return movies[movies['movieId'].isin(sim_scores.index)][['title', 'genres']]
+    #  SCORES CONTENT 
+    content_scores = pd.Series(
+        cosine_sim[movie_idx],
+        index=movies['movieId']
+    )
+
+    #  SCORES COLLABORATIVE 
+    if movie_id in movie_similarity_df.columns:
+        collaborative_scores = movie_similarity_df[movie_id]
+    else:
+        collaborative_scores = pd.Series(0, index=movies['movieId'])
+
+    #  SCORE HYBRIDE 
+    hybrid_scores = (
+        w_content * content_scores +
+        w_collaborative * collaborative_scores
+    )
+
+    # enlever le film lui-même
+    hybrid_scores = hybrid_scores.drop(movie_id)
+
+    # top N
+    top_movies = hybrid_scores.sort_values(ascending=False).head(n)
+
+    return movies[movies['movieId'].isin(top_movies.index)][['title', 'genres']]
 
 
 # Interface Streamlit
@@ -59,7 +94,7 @@ movie_selected = st.selectbox(
 )
 
 if st.button("Recommander"):
-    recommendations = recommend_movies_collaborative(movie_selected)
+    recommendations = recommend_movies_hybrid(movie_selected)
 
     if recommendations is None:
         st.error("Film non trouvé")
